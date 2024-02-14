5c274e1c495ad2d898653ae07251a99bf0c70bf9
Baha Jaghoob <bahajaghoob@gmail.com>
2024-02-14 13:42:31 +0200

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,54 @@
+package project1;
+
+public class Account {
+	private int ID;
+	private String holderName;
+	private long phoneNumber;
+	private char accountType;
+	 		double balance;
+	public void setId(int ID) {
+			this.ID=ID;
+
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setholderName(String holderName) {
+		 this.holderName=holderName;
+	}
+	public String getholderName() {
+		return holderName;
+	}
+	public void setphoneNumber(long phoneNumber) {
+		this.phoneNumber=phoneNumber;
+	}
+	public long getphoneNumber() {
+		return phoneNumber;
+		
+	}
+	public void setaccountType(char accountType) {
+		this.accountType=accountType;
+	}
+	public char getaccountType() {
+		return accountType;
+	}
+	public void setBalance(double balance) {
+		this.balance=balance;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	//setters and getters for account information
+}
+/*
+ * Made By:
+ * Bahaa Bani Shamsaa
+ * 1220252
+ * Abdallah hosam alqasarwi
+ * 1221432
+ */
+
+
+
+
+
